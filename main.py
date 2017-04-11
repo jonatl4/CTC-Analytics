fd86ca9e976e3168dda5f8105c516872c8de747e
jun-xue <junlianx@uci.edu>
2017-04-10 18:28:31 -0700

    added method to get accounts

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ my_app_secret = 'c793d8993eca253282938ce1e48a6bbd'
 my_access_token = 'EAAC1xqY89SIBAEvs4ycf8tK3AUH3ZBNEs6ZBDDsfyBlu7jGBPfVf96xqBJ3VVE3VdihZAonHnzvu19ZC3TzsfV3ES6CLJ5bh6XZCGwXcmu2etypGsK7xiyH1wZBEZAFggyspSdfw7QNYLmew6sUBoORkfPZAuOEpasEZD'
 
 
-
+def get_accounts():
+    """Retrieves and displays a list of the user's ad accounts."""
+    me = objects.AdUser(fbid='me')
+    my_accounts = list(me.get_ad_accounts(fields=[
+                'id',
+                'name',
+                'timezone_name',
+                'amount_spent',
+                'currency']))
+    print my_accounts
 
 
 
@@ -40,7 +49,8 @@ my_access_token = 'EAAC1xqY89SIBAEvs4ycf8tK3AUH3ZBNEs6ZBDDsfyBlu7jGBPfVf96xqBJ3V
 
 def main():
     FacebookAPI = FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
-    print FacebookAPI
+    #print FacebookAPI
+    get_accounts()
 
 
 
