c8f27a73035ba33b2bae02069a5b9efa3a93496d
jonatl4 <jonatl4@uci.edu>
2017-04-12 05:50:26 -0700

    Added get_ad_insights; removed get_ad_accounts
    Warning: Gets rate limited by Facebook API (Error #17)
    
    Need to find a more effective way to call API

--- main.py
+++ main.py
@@ -2,47 +2,58 @@ from facebookads.api import FacebookAdsApi
 from facebookads import objects
 from facebookads.adobjects.adaccount import AdAccount
 from facebookads.adobjects.adset import AdSet
+from facebookads.adobjects.campaign import Campaign
+from facebookads.adobjects.adsinsights import AdsInsights
+
+import datetime
 
 #Global variable for API Access 
 my_app_id = '199864797164834'
 my_app_secret = 'c793d8993eca253282938ce1e48a6bbd'
-my_access_token = 'EAAC1xqY89SIBAEvs4ycf8tK3AUH3ZBNEs6ZBDDsfyBlu7jGBPfVf96xqBJ3VVE3VdihZAonHnzvu19ZC3TzsfV3ES6CLJ5bh6XZCGwXcmu2etypGsK7xiyH1wZBEZAFggyspSdfw7QNYLmew6sUBoORkfPZAuOEpasEZD'
-
-
-def get_accounts():
-    """Retrieves and displays a list of the user's ad accounts."""
+my_access_token = 'EAAC1xqY89SIBANGlR56lthPzvV4jk0Gd0XwEZAsXYff0d6MQl1qd6FDS1yiOIFQ7ug7zhP6X1NdDU1ff3OvlqM49jSs5ZApFZBRmw5GPOahE53ikTCBwfWWhetohYiExJHORnxUFuZBTCsqkrl91f0mP2etXQoMZD'
+    
+def get_ad_insights():
+    
+    ### Set up user and read obj from server
     me = objects.AdUser(fbid='me')
-    my_accounts = list(me.get_ad_accounts(fields=[
+    my_accounts = me.get_ad_accounts(fields=[
                 'id',
-                'name',
-                'timezone_name',
-                'amount_spent',
-                'currency']))
-    print my_accounts
+                'name'])
 
+    ### Iterate through all ad accounts
+    for account in my_accounts:
+        print(account)
+        
+        for ad in account.get_ad_sets():
+            
+            ### Set the period of time (Default = 1 week)
+            today = datetime.date.today()
+            start_time = str(today - datetime.timedelta(weeks=40))
+            end_time = str(today)
+            fields = ['account_name',
+                      'ad_name',
+                      'adset_name',
+                      'campaign_name',
+                      'clicks',
+                      'cpc',
+                      'ctr',
+                      'frequency',
+                      'impressions',
+                      'reach',
+                      'cpm',
+                      'relevance_score']
+            
+            params = {
+                'time_range': {
+                    'since': start_time,
+                    'until': end_time,
+                },
+                'level': AdsInsights.Level.ad
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+            
+            insights = ad.get_insights(fields = fields, params = params)
+            print(insights)
 
 
 
@@ -50,8 +61,7 @@ def get_accounts():
 def main():
     FacebookAPI = FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
     #print FacebookAPI
-    get_accounts()
-
+    get_ad_insights()
 
 
 
