5353a3bcec70e1db6d731501662d3199e326b397
wyassine <wyassine@uci.edu>
2017-04-16 18:37:26 -0700

    start of program

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-from facebookads.api import FacebookAdsApi
-from facebookads import objects
-from facebookads.adobjects.adaccount import AdAccount
-from facebookads.adobjects.adset import AdSet
-
-#Global variable for API Access 
-my_app_id = '199864797164834'
-my_app_secret = 'c793d8993eca253282938ce1e48a6bbd'
-my_access_token = 'EAAC1xqY89SIBAEvs4ycf8tK3AUH3ZBNEs6ZBDDsfyBlu7jGBPfVf96xqBJ3VVE3VdihZAonHnzvu19ZC3TzsfV3ES6CLJ5bh6XZCGwXcmu2etypGsK7xiyH1wZBEZAFggyspSdfw7QNYLmew6sUBoORkfPZAuOEpasEZD'
-
-
-def get_accounts():
-    """Retrieves and displays a list of the user's ad accounts."""
-    me = objects.AdUser(fbid='me')
-    my_accounts = list(me.get_ad_accounts(fields=[
-                'id',
-                'name',
-                'timezone_name',
-                'amount_spent',
-                'currency']))
-    print my_accounts
+from modules.api import FacebookAPI
 
 
 
@@ -26,32 +6,27 @@ def get_accounts():
 
 
 
+def create_structure(facebook_ad_accounts):
+    """Returns a dictionary of the ad accounts with the following format {AccountName1: AccountID1, AccountName2: AccountID2...}"""
+    ad_account_ids = ['1647162208927124', '1656102788033066', '1699751017001576', '1379395989021046', '10100208564192664']
+    ad_account = {}
+    for acc in facebook_ad_accounts:
+        acc_strip = acc['id'].strip('act_')
+        if acc_strip in ad_account_ids:
+            ad_account[acc['name']] = acc['id']
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return ad_account
 
 
 
 def main():
-    FacebookAPI = FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
-    #print FacebookAPI
-    get_accounts()
-
+    """The controller of the entire program. It will handle initializing the facebook API connection, and call various modules in the program to perform tasks such as 
+        grabbing the facebook ad accounts, storing values in the database, generating the html document and sending the html document to a list of clients"""
+    FacebookConnection = FacebookAPI()
+    FacebookAdAccounts = create_structure(FacebookConnection.get_accounts())
+    for acc_name, acc_id in FacebookAdAccounts.items():
+        print acc_name
+        #acc_campaign_id = FacebookConnection.get_prospecting_campaign_id(acc_id)
 
 
 
---
+++ modules/api.py
@@ -0,0 +1,38 @@
+from facebookads.api import FacebookAdsApi
+from facebookads import objects
+from facebookads.adobjects.adaccount import AdAccount
+from facebookads.adobjects.adset import AdSet
+from facebookads.adobjects.campaign import Campaign
+
+#Global variable for API Access 
+my_app_id = '199864797164834'
+my_app_secret = 'c793d8993eca253282938ce1e48a6bbd'
+my_access_token = 'EAAC1xqY89SIBANGlR56lthPzvV4jk0Gd0XwEZAsXYff0d6MQl1qd6FDS1yiOIFQ7ug7zhP6X1NdDU1ff3OvlqM49jSs5ZApFZBRmw5GPOahE53ikTCBwfWWhetohYiExJHORnxUFuZBTCsqkrl91f0mP2etXQoMZD'
+
+
+
+class FacebookAPI:
+    def __init__(self):
+        FacebookAPI = FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
+
+    def get_accounts(self):
+        """Retrieves and displays a list of all user's ad accounts."""
+        me = objects.AdUser(fbid="me")
+        my_accounts = list(me.get_ad_accounts(fields=[
+            'id',
+            'name',
+            'timezone_name',
+            'amount_spent',
+            'currency']))
+        return my_accounts
+
+    def get_prospecting_campaign_id(self, acc_id):
+        """Takes in an account id and searches across the ad accounts campaigns looking for the campaign name Prospecting. If found, return the campaign id."""
+        account = AdAccount(acc_id).get_campaigns()
+        for camp in account:
+            campaign =  Campaign(camp['id'])
+            campaign.remote_read(fields=[
+                Campaign.Field.name,
+            ])
+            if campaign['name'] == 'Prospecting':
+                return camp['id']
