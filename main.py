b9685de2303ccbf9473f08cb55ebf1014781df6d
jakedw <jakedw@uci.edu>
2017-04-16 23:00:04 -0700

    CTC Algorithm First Iteration

--- main.py
+++ main.py
@@ -6,7 +6,83 @@ from modules.api import FacebookAPI
 def calculate_ctc_score(campaign_stats):
     """Returns a ctc score from a given campaign"""
     print campaign_stats
-    return
+
+        ctrScore, cpcScore, clickScore, frequencyScore = 0, 0, 0, 0
+    
+    for keys in campaign_stats:
+        for value in keys:
+            
+            """The percentage of times people saw your ad and performed a click"""              
+            if value == 'ctr':
+                if keys[value] < 1:
+                    ctrScore = 0.2
+                elif keys[value] > 1 and keys[value] < 3:
+                    ctrScore = 0.4
+                elif keys[value] > 3 and keys[value] < 6:
+                    ctrScore = 0.65
+                elif keys[value] > 6 and keys[value] < 8:
+                    ctrScore = 0.85
+                elif keys[value] > 8:
+                    ctrScore = 1
+                
+            """The average cost for each click"""    
+            if value == 'cpc':
+                if keys[value] < 0.25:
+                    cpcScore = 0.2
+                elif keys[value] > 0.25 and keys[value] < 0.50:
+                    cpcScore = 0.5
+                elif keys[value] > 0.50:
+                    cpcScore = 0.7     
+                    
+            """The number of clicks on your ads"""
+            if value == 'clicks':
+                if keys[value] < 1000:
+                    clickScore = 0
+                elif keys[value] > 1000 and keys[value] < 10000:
+                    clickScore = 0.2
+                elif keys[value] > 10000 and keys[value] < 50000:
+                    clickScore = 0.3
+                elif keys[value] > 50000 and keys[value] < 200000:
+                    clickScore = 0.5
+                elif keys[value] > 200000 and keys[value] < 300000:
+                    clickScore = 0.7
+                elif keys[value] > 300000 and keys[value] < 500000:
+                    clickScore = 0.9
+                elif keys[value] > 500000:
+                    clickScore = 1
+                  
+            """The average number of times each person saw your ad"""  
+            if value == 'frequency':
+                if keys[value] < 1:
+                    frequencyScore = 0.2
+                elif keys[value] > 1 and keys[value] < 3:
+                    frequencyScore = 0.6
+                elif keys[value] > 3 and keys[value] < 6:
+                    frequencyScore = 0.9
+                elif keys[value] > 6 and keys[value] < 9:
+                    frequencyScore = 1
+                elif keys[value] > 9 and keys[value] < 12:
+                    frequencyScore = 0.7
+                elif keys[value] > 12:
+                    frequencyScore = 0.4
+
+        """Score * (Weight) determines how much influence a specific field has on the algorithm.
+        Score = a we come up with based on the data
+        Weight = how important we think that field is in making a decision (out of 1)
+        numberOfFields = the number of fields that will give us a number between 0 and 1 when divided
+        """
+    numberOfFields = 4
+    
+    finalAlgorithm = (ctrScore*(.35) 
+                      + cpcScore*(.30) 
+                      + clickScore*(.25) 
+                      + frequencyScore*(.10) 
+                      )/ numberOfFields
+    
+    print('CTC SCORE: {}'.format(finalAlgorithm))
+    return finalAlgorithm
+
+
 
 
 def create_structure(facebook_ad_accounts):
