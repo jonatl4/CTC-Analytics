40f77a175de3e88e4b71c990f6259814fc417898
jakedw <jakedw@uci.edu>
2017-05-01 05:36:43 -0700

    added stats module

---
+++ modules/algstats.py
@@ -0,0 +1,26 @@
+
+"""Calculates the standard deviation of given data sets that is used by CTC Score Algorithm
+Normally included in latest Python versions, but not 2.7 so it must be calculated here manually."""
+
+
+def mean(data):
+    """Return the sample arithmetic mean of data."""
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+    return sum(data)/float(n)
+
+def _ss(data):
+    """Return sum of square deviations of sequence data."""
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+def pstdev(data):
+    """Calculates the population standard deviation."""
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/(n-1) # the sample variance
+    return pvar**0.5
